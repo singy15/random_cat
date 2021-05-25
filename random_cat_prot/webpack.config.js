63ccf01ba42c3d548a415e6bfe2a9c2fe3edc1a2
kedama <uesastorage3@gmail.com>
2021-05-25 09:45:02 +0900

    Delete comment

--- random_cat_prot/webpack.config.js
+++ random_cat_prot/webpack.config.js
@@ -15,14 +15,6 @@ module.exports = {
   // },
   entry: {
     "index": "./src/js/index.js"
-    // workplan: "./src/js/workplan.js"
-    // ,"gantt-style": "./src/js/gantt-style.js"
-    // ,"user-master": "./src/js/user-master.js"
-    // ,signin: "./src/js/signin.js"
-    // ,"solution-master": "./src/js/solution-master.js"
-    // ,"core_util": "./src/js/core_util.js"
-    // ,"constants": "./src/js/constants.js"
-    // ,"workplan-report": "./src/js/workplan-report.js"
   },
   output: {
     path: path.join(__dirname, "dist"),
@@ -55,12 +47,6 @@ module.exports = {
       patterns: [
         { from: 'src/html', to: "" } 
         ,{ from: 'src/static', to: "static" } 
-        // ,{ from: 'src/favicon.ico', to: "public/favicon.ico" } 
-
-        // // Deploy for wasp_cl
-        // ,{ from: 'src/public', to: "../../wasp_cl/document-root/public" } 
-        // ,{ from: 'dist/public', to: "../../wasp_cl/document-root/public" } 
-        // ,{ from: 'src/html', to: "../../wasp_cl/templates" } 
       ],
       options: {
         concurrency: 1
