8fdec8ed6899e8ec9f7cdeba5c79cdd56073be7d
kedama <uesastorage3@gmail.com>
2021-05-28 15:58:51 +0900

    fix layout

--- src/php/views/signup_success.blade.php
+++ src/php/views/signup_success.blade.php
@@ -6,22 +6,24 @@
 
 @section('content')
   <div class="container">
+    <div class="row">
       <div class="col-12 col-md-9 col-lg-8 mx-auto">
-      <div class="col">
-          <span class="text-muted">
-            アカウント作成に成功しました。<br>
-            ログイン画面からログインしてください。
-          </span>
+        <span class="text-muted">
+          アカウント作成に成功しました。<br>
+          ログイン画面からログインしてください。
+        </span>
+      </div>
+    </div>
 
-          <a href="/login.php" class="labeled-image-button 
-              text-x-small text-bold text-color-gray">
-            <img class="labeled-image-button" width="64" height="64" 
-                src="/static/image/login2.svg" alt="ログイン">
-            <br>
-            <span>ログイン</span>
-          </a>
-        </form>
-
+    <div class="row mt-3">
+      <div class="col-12 col-md-9 col-lg-8 mx-auto">
+        <a href="/login.php" class="labeled-image-button 
+            text-x-small text-bold text-color-gray">
+          <img class="labeled-image-button" width="64" height="64" 
+              src="/static/image/login2.svg" alt="ログイン">
+          <br>
+          <span>ログイン</span>
+        </a>
       </div>
     </div>
   </div>
