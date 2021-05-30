d2aef9de1e55f8aa436a1b49bc6b046fe69007d3
kedama <uesastorage3@gmail.com>
2021-05-30 22:33:23 +0900

    fix migration

--- database/flyway/sql/V0.0.2.0__create_table_favorite.sql
+++ database/flyway/sql/V0.0.2.0__create_table_favorite.sql
@@ -4,7 +4,7 @@ drop table if exists favorite;
 
 --* RestoreFromTempTable
 create table favorite (
-  favorite_id INTEGER not null AUTOINCREMENT
+  favorite_id INTEGER not null
   , user_id INTEGER not null
   , post_id INTEGER not null
   , constraint favorite_PKC primary key (favorite_id)
