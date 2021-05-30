78bba2e541172e8f11a60b9330db438da1917dc7
kedama <uesastorage3@gmail.com>
2021-05-30 19:56:57 +0900

    jump to login when not logged in

--- src/php/views/index.blade.php
+++ src/php/views/index.blade.php
@@ -13,12 +13,17 @@
     <div class="row">
       <div class="col-12 col-md-9 col-lg-8 mx-auto">
         <div class="card shadow-sm p-1">
-          <img class="bd-placeholder-img card-img-top img-fluid" src="/static/photo/{{ $post->photo_path }}" role="img" 
+          <img class="bd-placeholder-img card-img-top img-fluid" 
+            src="/static/photo/{{ $post->photo_path }}" role="img" 
             aria-label="Placeholder: Thumbnail"  focusable="false">
-            <span class="text-bold text-color-gray">"{{ $post->subject }}"</span>
+            <span class="text-bold text-color-gray">
+              "{{ $post->subject }}"
+            </span>
           </img>
           <div class="card-body py-1 pb-2">
-            <p class="card-text small text-color-gray">{{ $post->description }}</p>
+            <p class="card-text small text-color-gray">
+              {{ $post->description }}
+            </p>
             <div class="d-flex justify-content-between align-items-center">
               <small class="text-muted">投稿者: {{ $author->login_id }}</small>
 
@@ -27,7 +32,7 @@
                     name="post_form_favorite" style="display:inline">
                   <input type="hidden" name="post_id" 
                       value="{{ $post->post_id }}">
-                  <a href="javascript:toggleFavorite({{ $post->post_id }})" 
+                  <a href="{{ isset($_SESSION['user_id'])? 'javascript:toggleFavorite('. $post->post_id .')' : '/login.php' }}" 
                       class="clear-link">
                     <img id="favorite" 
                       src="{{ ($favorite)? '/static/image/heart.svg' : '/static/image/heart-outlined.svg' }}" 
@@ -42,7 +47,7 @@
                     name="post_form_like" style="display:inline">
                   <input type="hidden" name="post_id" 
                       value="{{ $post->post_id }}">
-                  <a href="javascript:toggleLike({{ $post->post_id }})" 
+                  <a href="{{ isset($_SESSION['user_id'])? 'javascript:toggleLike('. $post->post_id .')' : '/login.php' }}" 
                       class="clear-link">
                     <img id="like" 
                       src="{{ ($like)? '/static/image/footprint-filled.svg' : '/static/image/footprint-outlined.svg' }}" 
@@ -62,20 +67,26 @@
 
     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 py-2">
       <div class="col">
-        <a href="/index.php" class="labeled-image-button text-x-small text-bold text-color-gray">
-          <img class="labeled-image-button" width="64" height="64" src="/static/image/cat-walk.svg" alt="もっとみる">
+        <a href="/index.php" class="labeled-image-button text-x-small 
+            text-bold text-color-gray">
+          <img class="labeled-image-button" width="64" height="64" 
+            src="/static/image/cat-walk.svg" alt="もっとみる">
           <br>
           <span>もっとみる</span>
         </a>
 
-        <a href="{{ isset($_SESSION['user_id'])? '/upload.php' : '/login.php' }}" class="labeled-image-button text-x-small text-bold text-color-gray">
-          <img class="labeled-image-button" width="64" height="64" src="/static/image/upload.svg" alt="投稿にゃんこ">
+        <a href="{{ isset($_SESSION['user_id'])? '/upload.php' : '/login.php' }}" 
+           class="labeled-image-button text-x-small text-bold text-color-gray">
+          <img class="labeled-image-button" width="64" height="64" 
+            src="/static/image/upload.svg" alt="投稿にゃんこ">
           <br>
           <span>投稿する</span>
         </a>
 
-        <a href="{{ isset($_SESSION['user_id'])? '/my_nyanko.php' : '/login.php' }}" class="labeled-image-button text-x-small text-bold text-color-gray">
-          <img class="labeled-image-button" width="64" height="64" src="/static/image/heart.svg" alt="Myにゃんこ">
+        <a href="{{ isset($_SESSION['user_id'])? '/my_nyanko.php' : '/login.php' }}" 
+          class="labeled-image-button text-x-small text-bold text-color-gray">
+          <img class="labeled-image-button" width="64" height="64" 
+            src="/static/image/heart.svg" alt="Myにゃんこ">
           <br>
           <span>Myにゃんこ</span>
         </a>
